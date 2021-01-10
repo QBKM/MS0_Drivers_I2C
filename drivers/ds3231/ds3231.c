c4bd4e3fb4d4c13f09fe0f453317a0a06f59b61f
Quentin Bakrim <quentin.bakrim@hotmail.fr>
2021-01-10 18:39:04 +0100

    driver for ds3231 on STM32

---
+++ drivers/ds3231/ds3231.c
@@ -0,0 +1,147 @@
+/*
+ * ds3231.c
+ *
+ *  Created on: 4 janv. 2021
+ *      Author: Quent
+ */
+
+
+/* ------------------------------------------------------------- --
+   Includes
+-- ------------------------------------------------------------- */
+#include "ds3231.h"
+
+
+/* ------------------------------------------------------------- --
+   Variables
+-- ------------------------------------------------------------- */
+static const uint16_t i2c_timeout = 100;
+
+
+/* ------------------------------------------------------------- --
+   private prototypes
+-- ------------------------------------------------------------- */
+static uint8_t B2D(uint8_t bcd);
+static uint8_t D2B(uint8_t decimal);
+
+
+/* ============================================================= ==
+   Functions
+== ============================================================= */
+/* ************************************************************* *
+ * @name		DS3231_Init
+ * @brief
+ *
+ * @args
+ * ************************************************************* */
+uint8_t DS3231_Init(DS3231_t *DS3231)
+{
+  DS3231->Sec =         0;  /* 0 sec */
+  DS3231->Min =         0;  /* 0 mins */
+  DS3231->Hour =        0;  /* 0 hours */
+  DS3231->DaysOfWeek =  1;  /* monday*/
+  DS3231->Date =        1;  /* 1st */
+  DS3231->Month =       1;  /* January */
+  DS3231->Year =        0;  /* 0000 */
+
+  if(DS3231_SetTime(DS3231)) return HAL_ERROR;
+	return HAL_OK;
+}
+
+/* ************************************************************* *
+ * @name		DS3231_Read_All
+ * @brief
+ *
+ * @args
+ * ************************************************************* */
+uint8_t DS3231_Read_All(DS3231_t *DS3231)
+{
+  if(DS3231_Read_Time(DS3231)) return HAL_ERROR;
+  if(DS3231_Read_Temperature(DS3231)) return HAL_ERROR;
+
+  return HAL_OK;
+}
+
+/* ************************************************************* *
+ * @name		DS3231_Read_Time
+ * @brief
+ *
+ * @args
+ * ************************************************************* */
+uint8_t DS3231_Read_Time(DS3231_t *DS3231)
+{
+  uint8_t data[7];
+
+  if(HAL_I2C_Mem_Read(&DS3231->i2c, DS3231_ADDR, DS3231_REG_TIME, 1, data, sizeof(data), i2c_timeout )) return HAL_ERROR;
+
+  DS3231->Sec = B2D(data[0] & 0x7F);
+  DS3231->Min = B2D(data[1] & 0x7F);
+  DS3231->Hour = B2D(data[2] & 0x3F);
+  DS3231->DaysOfWeek = data[3] & 0x07;
+  DS3231->Date = B2D(data[4] & 0x3F);
+  DS3231->Month = B2D(data[5] & 0x1F);
+  DS3231->Year = B2D(data[6]);
+
+  return HAL_OK;
+}
+
+
+/* ************************************************************* *
+ * @name		DS3231_SetTime
+ * @brief
+ *
+ * @args
+ * ************************************************************* */
+uint8_t DS3231_SetTime(DS3231_t *DS3231)
+{
+  uint8_t startAddr = DS3231_REG_TIME;
+  uint8_t data[8] = {startAddr, D2B(DS3231->Sec), D2B(DS3231->Min), D2B(DS3231->Hour), DS3231->DaysOfWeek, D2B(DS3231->Date), D2B(DS3231->Month), D2B(DS3231->Year)};
+  
+  if(HAL_I2C_Master_Transmit(&DS3231->i2c, DS3231_ADDR, data, sizeof(data), HAL_MAX_DELAY)) return HAL_ERROR;
+
+  return HAL_OK;
+}
+
+
+/* ************************************************************* *
+ * @name		DS3231_Read_Temperature
+ * @brief
+ *
+ * @args
+ * ************************************************************* */
+uint8_t DS3231_Read_Temperature(DS3231_t *DS3231)
+{
+  uint8_t data[2];
+
+  if(HAL_I2C_Mem_Read(&DS3231->i2c, DS3231_ADDR, DS3231_REG_TEMP, 1, data, sizeof(data), i2c_timeout )) return HAL_ERROR;
+  
+  int16_t value = (data[0] << 8) | (data[1]);
+  value = (value >> 6);
+
+  DS3231->temperature = (value / 4.0f);
+  return HAL_OK;
+}
+
+
+/* ************************************************************* *
+ * @name		B2D
+ * @brief bcd to decimal
+ *
+ * @args
+ * ************************************************************* */
+static uint8_t B2D(uint8_t bcd)
+{
+  return (bcd >> 4) * 10 + (bcd & 0x0F);
+}
+
+
+/* ************************************************************* *
+ * @name		D2B
+ * @brief   decimal to bcd
+ *
+ * @args
+ * ************************************************************* */
+static uint8_t D2B(uint8_t decimal)
+{
+  return (((decimal / 10) << 4) | (decimal % 10));
+}
---
+++ drivers/ds3231/ds3231.h
@@ -0,0 +1,122 @@
+/*
+ * ds3231.h
+ *
+ *  Created on: 4 janv. 2021
+ *      Author: Quent
+ */
+
+#ifndef __DS3231_H__
+#define __DS3231_H__
+
+
+/* ------------------------------------------------------------- --
+   Includes
+-- ------------------------------------------------------------- */
+#include "main.h"
+#include <stdlib.h>
+#include "stdarg.h"
+#include <stdbool.h>
+
+
+/* ------------------------------------------------------------- --
+   Define
+-- ------------------------------------------------------------- */
+/* DS3231 address */
+#define DS3231_ADDR  (0x68 << 1)
+
+/* DS3231 registers */
+#define DS3231_REG_TIME         0x00
+#define DS3231_REG_ALARM1       0x07
+#define DS3231_REG_ALARM2       0x0B
+#define DS3231_REG_CONTROL      0x0E
+#define DS3231_REG_STATUS       0x0F
+#define DS3231_REG_TEMP         0x11
+
+#define DS3231_CON_EOSC         0x80
+#define DS3231_CON_BBSQW        0x40
+#define DS3231_CON_CONV         0x20
+#define DS3231_CON_RS2          0x10
+#define DS3231_CON_RS1          0x08
+#define DS3231_CON_INTCN        0x04
+#define DS3231_CON_A2IE         0x02
+#define DS3231_CON_A1IE         0x01
+
+#define DS3231_STA_OSF          0x80
+#define DS3231_STA_32KHZ        0x08
+#define DS3231_STA_BSY          0x04
+#define DS3231_STA_A2F          0x02
+#define DS3231_STA_A1F          0x01
+
+
+/* ------------------------------------------------------------- --
+   Enumerates
+-- ------------------------------------------------------------- */
+typedef enum
+{
+  ALARM_MODE_ALL_MATCHED = 0,
+  ALARM_MODE_HOUR_MIN_SEC_MATCHED,
+  ALARM_MODE_MIN_SEC_MATCHED,
+  ALARM_MODE_SEC_MATCHED,
+  ALARM_MODE_ONCE_PER_SECOND
+} DS3231_AlarmMode;
+
+typedef enum
+{
+  SUNDAY = 1,
+  MONDAY,
+  TUESDAY,
+  WEDNESDAY,
+  THURSDAY,
+  FRIDAY,
+  SATURDAY
+} DS3231_DaysOfWeek;
+
+
+/* ------------------------------------------------------------- --
+   Structures
+-- ------------------------------------------------------------- */
+typedef struct
+{
+   uint8_t Year;
+   uint8_t Month;
+   uint8_t Date;
+   uint8_t DaysOfWeek;
+   uint8_t Hour;
+   uint8_t Min;
+   uint8_t Sec;
+
+   float temperature;
+
+   I2C_HandleTypeDef i2c;
+}DS3231_t;
+
+
+typedef struct
+{
+   uint8_t Year;
+   uint8_t Month;
+   uint8_t Date;
+   uint8_t DaysOfWeek;
+   uint8_t Hour;
+   uint8_t Min;
+   uint8_t Sec;
+}DS3231_config_t;
+
+/* ------------------------------------------------------------- --
+   Functions
+-- ------------------------------------------------------------- */
+uint8_t DS3231_Init(DS3231_t *DS3231);
+
+uint8_t DS3231_Read_All(DS3231_t *DS3231);
+uint8_t DS3231_Read_Time(DS3231_t *DS3231);
+uint8_t DS3231_Read_Temperature(DS3231_t *DS3231);
+
+uint8_t DS3231_SetTime(DS3231_t *DS3231);
+
+
+#endif /* __DS3231_H__ */
+
+
+/* ------------------------------------------------------------- --
+   End of files
+-- ------------------------------------------------------------- */
